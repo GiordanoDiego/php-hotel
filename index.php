d70b2ee3ce455d29c0192962b5ff90b9a4fc59ca
GiordanoDiego <diegomgiordano96@gmail.com>
2024-02-06 15:20:26 +0100

    step 1

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -17,7 +58,9 @@
         <!-- START MAIN HEADER -->
         <header>
             <div class="container">
-                HEAD
+                <h1 class=" text-center ">
+                    Hotel
+                </h1>
             </div>
         </header>
         <!-- END MAIN HEADER -->
@@ -25,18 +68,19 @@
         <!-- START MAIN -->
         <main>
             <div class="container">
-                MAIN
+                <div class="hotels_container">
+                    <ul>
+                        <?php 
+                            foreach ($hotels as $hotel){
+                                echo "<li>".$hotel["name"]."</li>";
+                            }
+                        ?>
+                    </ul>
+                </div>
             </div>
         </main>
         <!-- END MAIN -->
 
-        <!-- START FOOTER -->
-        <footer>
-            <div class="container">
-                FOOTER
-            </div>
-        </footer>
-        <!-- END FOOTER -->
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="./js/script.js"></script>
